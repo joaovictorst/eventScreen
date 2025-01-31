0db109f8236cbd9cce3dc4125ad5d89ef99d636f
joaovictorst <joaunvictor23@hotmail.com>
2025-01-31 16:38:21 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+let input1 = `S;M;plasticCup()
+
+C;V;mobile phone
+
+C;C;coffee machine
+
+S;C;LargeSoftwareBook
+
+C;M;white sheet of paper
+
+S;V;pictureFrame`
+
+
+let input1tratado = input1.split(/[\r\n]+/)
+
+console.log(input1tratado)
